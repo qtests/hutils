10f7d840222f4f21c9dbef8fffd4930dab43b86f
qtests <qtests@outlook.com>
2018-08-16 12:52:02 +0200

    INIParser

--- src/Main.hs
+++ src/Main.hs
@@ -1,5 +1,14 @@
 module Main where
 
-main :: IO ()
-main = do
-  putStrLn "hello world"
+  import System.IO
+  import System.Environment
+
+  import Parsers.INIParser
+  
+  main :: IO ()
+  main = do
+    args <- getArgs
+    contents <- readFile (head args)
+    case runParser ini contents of
+      Just inicontents -> print inicontents
+      Nothing          -> putStrLn "Could not parse INI file"
