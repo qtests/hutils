9c6b4ab0410a749c9e9cd36eb182166549f89b74
qtests <qtests@outlook.com>
2018-08-24 12:18:22 +0200

    BitsPieces - CodePieces

---
+++ src/BitsPieces/CodePieces.hs
@@ -0,0 +1,14 @@
+module BitsPieces.CodePieces
+(
+    nubOrd
+)
+where
+
+import qualified Data.Set as Set
+
+nubOrd :: Ord a => [a] -> [a] 
+nubOrd xs = go Set.empty xs where
+  go s (x:xs)
+   | x `Set.member` s = go s xs
+   | otherwise        = x : go (Set.insert x s) xs
+  go _ _              = []
