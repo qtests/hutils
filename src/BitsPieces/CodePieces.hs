93c4e8f06b5bd616f3ff70d452e9d01e2e95ec31
qtests <qtests@outlook.com>
2018-08-24 12:19:44 +0200

    link

--- src/BitsPieces/CodePieces.hs
+++ src/BitsPieces/CodePieces.hs
@@ -6,6 +6,7 @@ where
 
 import qualified Data.Set as Set
 
+-- https://stackoverflow.com/questions/3098391/unique-elements-in-a-haskell-list
 nubOrd :: Ord a => [a] -> [a] 
 nubOrd xs = go Set.empty xs where
   go s (x:xs)
